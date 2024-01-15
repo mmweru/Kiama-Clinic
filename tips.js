e0839e15f17020042083844ff2c1426590292ea0
Maryann Mweru <mwerumaryann@gmail.com>
2024-01-16 01:12:48 +0300

    Add swiper in smaller screens

--- tips.js
+++ tips.js
@@ -3,18 +3,25 @@ import smart from 'smart-animate';
 
 const swiper = new Swiper('.swiper', {
   spaceBetween: 30,
-  slidesPerView: 1, /* Show one card at a time */
+  slidesPerView: 1,
   loop: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    // Adjust breakpoint if needed
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
   },
   on: {
     init: () => {
       const slides = swiper.slides;
       slides.forEach((slide, index) => {
         smart(slide, {
-          animate: 'fadeInOut', /* Use fadeInOut for smooth animation */
+          animate: 'fadeInOut',
           duration: 1500,
           delay: index * 1500,
         });
